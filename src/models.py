6acf1fe9c83070af159a830f9746041e3f4a1bca
Xavier Longo Dios <45659915+Longodios@users.noreply.github.com>
2024-06-30 12:16:27 +0000

    Ejercicio finalizado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable= False)
+    password = Column(String(250), nullable = False, unique = True)
+    email = Column(String(250), nullable=False, unique = True)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    character_id = Column(Integer,  primary_key= True, nullable = False)
+    name = Column(String(250), nullable = False)
+    age = Column(Integer)
+    description = Column(String(250))
+    gender = Column(String(20))
+    species = Column(String(20))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key = True)
+    name = Column(String(50), nullable = False)
+    distance = Column(Integer)
+    description = Column(String(250))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    vehicle_id = Column(Integer , primary_key = True, nullable = False)
+    name = Column(String(50), nullable = False)
+    model = Column(String(50), nullable = False)
+    created = Column(DateTime , nullable = False)
+    number_passengers = Column(Integer)
+    capacity = Column(Integer)
+
+
+
+
+class User_Favourites(Base):
+    __tablename__ = 'user_favourites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    saved_planets = Column(String(250), ForeignKey('planets.id'), nullable = False)
+    saved_characters = Column(String(250), ForeignKey ('characters.id'), nullable = False)
+    saved_vehicles = Column(String(250), ForeignKey("vehicles.id"),nullable=False)
+    user_favourites = relationship(User)
+    from_character = relationship(Characters)
+    from_planets = relationship(Planets)
+    from_vehicles = relationship(Vehicles)
 
     def to_dict(self):
         return {}
